df8720458c06e320f2175c90b0b4eeb1014ce76d
mahilmithranks <mahilmithranks2007@gmail.com>
2025-02-26 13:51:48 +0530

    Completed Assignment-2

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 
 connectDB();
--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const  MenuItem = require("../models/menuItem");
 
-
+//ASSESMENT-1
 router.get("/",async (req,res) => {
     try {
         const menuItems = await MenuItem.find();
@@ -27,6 +27,45 @@ router.post("/",async (req,res) => {
         res.status(201).json({message: "Menu item added successfully!!",data: newItem});
     } catch (error) {
         res.status(500).json({message: "Error adding menu item", error: error.message});
+        
+    }
+});
+
+
+//ASSESMENT-2
+
+router.put("/:id",async (req,res) => {
+    try {
+        const {id} = req.params;
+        const {name, description, price} = req.body;
+
+        const updatedItem = await MenuItem.findByIdAndUpdate(
+            id,
+            {name, description, price},
+            {new: true, runValidators: true}
+        );
+
+        if(!updatedItem){
+            return res.status(404).json({messgae: "menu item not found"});
+        }
+        res.status(200).json({message: "Menu item updated Successfully",data: updatedItem});
+    } catch (error) {
+        res.status(500).json({message: "Error updating menu item",error: error.message});
+        
+    }
+});
+
+router.delete("/:id",async (req,res) => {
+    try {
+        const {id} = req.params;
+        const deletedItem = await MenuItem.findByIdAndDelete(id);
+
+        if(!deletedItem){
+            return res.status(404).json({message: "Menu item not found"});
+        }
+        res.status(200).json({message: "Menu item deleted Successfully"});
+    } catch (error) {
+        res.status(500).json({message: "Error deleting menu item",error: error.message});
         
     }
 });
